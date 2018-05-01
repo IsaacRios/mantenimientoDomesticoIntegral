fe231d64da43f328279c30ddeacf8a5b66c48ead
Isaac <rios.isaac@webmonster.mx>
2018-05-01 14:36:15 -0500

    layout

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-var express = require('express')
+var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
+mongoose.connect("mongodb://localhost/mdijb");
+
+//Define el schema de los servicios
+var serviceSchema = {
+	title:String,
+	description:String,
+	imageUrl:String,
+	pricing:Number
+};
+
+var Service = mongoose.model("Service", serviceSchema);
+
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
 app.get("/",function(solicitud,respuesta){
-	respuesta.send("Hola mundo");
+	respuesta.render("index");
+});
+
+app.get("/servicios/new", function(solicitud,respuesta){
+	respuesta.render("servicios/new");
 });
 
 app.listen(8080);
